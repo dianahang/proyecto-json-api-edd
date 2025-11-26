a77971b92a10de610064be54a00516d74ac028eb
DianaHang <dianavegat99@gmail.com>
2025-11-25 20:07:12 -0600

    Jackson dependency's version

---
+++ src/main/java/api/LectorJSONJackson.java
@@ -0,0 +1,69 @@
+package api;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class LectorJSONJackson {
+    public static void main(String[] args) {
+        // Atributo (endpoint Rick y Morty API)
+        String endpoint = "https://rickandmortyapi.com/api/character";
+
+        try {
+            // Mandar a llamar métodos
+            String jsonString = leerAPI(endpoint);
+            imprimirJSONOrdenado(jsonString);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error de ejecución: \n" + e);
+        }
+    }
+
+    // Consumir API. Devuelve el JSON como String
+    public static String leerAPI(String urlString) throws Exception {
+        // Instancia para la petición al endpoint
+        URL url = new URL(urlString);
+        // Generar conexión a protocolo HTTP con un cast
+        HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
+        // Elección del método de consulta
+        conexion.setRequestMethod("GET");
+
+        // Validar Status Code
+        int status = conexion.getResponseCode();
+        if (status != 200) {
+            throw new RuntimeException("Error HTTP: " + status);
+        }
+
+        // Leer el flujo de bytes del input stream
+        InputStream input = conexion.getInputStream();
+        byte[] bytes = input.readAllBytes();
+
+        // Construir el contenido de JSON
+        StringBuilder jsonBuilder = new StringBuilder();
+        for (byte b : bytes) {
+            jsonBuilder.append((char) b);
+        }
+
+        conexion.disconnect();
+
+        return jsonBuilder.toString();
+    }
+
+    // Imprimir JSON en orden y con formato
+    public static void imprimirJSONOrdenado(String jsonString) throws Exception {
+
+        // Crear el ObjectMapper de Jackson
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Convertir el String JSON a un LinkedHashMap
+        JsonNode root = mapper.readTree(jsonString);
+
+        // Imprimir el JSON formateado
+        String jsonFormateado = mapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(root);
+
+        System.out.println(jsonFormateado);
+    }
+}
